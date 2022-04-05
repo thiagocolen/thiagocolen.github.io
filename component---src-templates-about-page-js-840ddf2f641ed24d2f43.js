098bb12dcb6ea67133ffe58590f6137394f09119
Thiago Colen <thiago.souzacolen@gmail.com>
2022-04-05 12:22:50 -0300

    Updates

---
+++ component---src-templates-about-page-js-840ddf2f641ed24d2f43.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkthiago_colen=self.webpackChunkthiago_colen||[]).push([[311],{6383:function(e,t,n){n.r(t);var a=n(7294),l=n(7562),c=n(4045),o=n(1039);t.default=function(e){var t=e.pageContext;return console.log("### About pageContext:",t),a.createElement(a.Fragment,null,a.createElement(l.Z,{activePage:"about"}),a.createElement(o.Z,null,a.createElement("h1",{className:"text-my-theme-3 text-3xl mb-6"},"About")),a.createElement(c.Z,null))}}}]);
+//# sourceMappingURL=component---src-templates-about-page-js-840ddf2f641ed24d2f43.js.map

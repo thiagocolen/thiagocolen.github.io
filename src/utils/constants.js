a584cce40c2f64ef3126a63aa354345874d0f23e
Thiago Colen <thiago.souzacolen@gmail.com>
2022-08-31 20:13:47 -0300

    adding two more song lines

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,5 +4,7 @@ export const songsSnippets = [
   "I said I bet that you look good on the dance floor. Dancing to electro-pop like a robot from 1984.",
   "A modern day warrior. Mean, mean stride. Today's Tom Sawyer. Mean, mean pride.",
   "As the world goes. The strong do what they can. And the weak suffer what they must. Shallow be thy graves.",
-  "All I ever wanted. All I ever needed is here in my arms. Words are very unnecessary. They can only do harm."
+  "All I ever wanted. All I ever needed is here in my arms. Words are very unnecessary. They can only do harm.",
+  "And I'm watchin' and I'm waitin' Hopin' for the best, Even think I'll go to prayin' Every time I hear 'em sayin' That there's no way to delay, That trouble comin' every day",
+  "My name is Mud, Not to be confused with Bill or Jack or Pete or Dennis, My name is Mud and it's always been"
 ];
